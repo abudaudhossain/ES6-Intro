d8a269652de0813042c4bd9fe13aed8f890be90a
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-23 16:19:21 +0600

    add ES-6 Introctoin

---
+++ js-recap.js
@@ -0,0 +1,41 @@
+// variable 
+var myAge = 23;
+
+// condition 
+if(myAge < 23){
+
+}else if(myAge > 23){
+
+}else if(myAge == 23){
+
+}else if(myAge != 24){
+    
+}else if(myAge >= 23){
+
+}else if(myAge <= 23){
+
+}else{
+
+}
+
+const numbers =[12,34,45,32,23]
+// looping
+// while and for 
+for(const num of numbers){
+
+}
+
+// function 
+function myName(firstName, lastName){
+    return fullName = firstName + ' '+ lastName;
+}
+const Name = myName('Abu','Daud');
+
+// Object 
+const myLeptop = {
+    name: "PH Pavilon",
+    price: 61000,
+    RAM: "8GB"
+}
+
+console.log(myLeptop.name);
