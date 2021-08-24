b4ae2c6af77e79283812b4e81fd0859ec626a7d7
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-24 08:20:42 +0600

    ad breck trik

---
+++ tamplate-string.js
@@ -0,0 +1,11 @@
+const firstName = 'Abu Daud';
+const lastName = 'Hossain';
+let fullName = firstName + ' ' + lastName;
+const fullName2 =`${firstName} ${lastName}`;
+
+const frients = ['Rakibu', 'sakibul', 'mohiblu', 'koribul'];
+const detials = `hi,
+I am ${firstName} ${lastName}. I live in Rowmari. 
+Now I am enroll in Progaramming Here.Here also 
+add My ${frients.length}. so there I am so happy. `
+console.log(detials);
